11af8e4a3bf2fcfbf3432c18471250bb2280050b
Jose Marquez <josesinmarquezpalmeros@gmail.com>
2020-03-24 17:38:49 -0500

    got the logout feature to work

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -3,6 +3,7 @@ import apiKeys from './helpers/apiKeys.json';
 
 import authData from './helpers/data/authData';
 import auth from './components/auth/auth';
+import myNavbar from './components/myNavbar/myNavbar';
 
 import '../styles/main.scss';
 import 'bootstrap';
@@ -11,6 +12,7 @@ const init = () => {
   firebase.initializeApp(apiKeys.firebaseKeys);
   authData.checkLoginStatus();
   auth.loginButton();
+  myNavbar.logoutEvent();
 };
 
 init();
