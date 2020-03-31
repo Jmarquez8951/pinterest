e1ee0a1f735acc9295ad24864a87a09f4638b0cc
Jose Marquez <josesinmarquezpalmeros@gmail.com>
2020-03-31 17:21:44 -0500

    got the board to be deleted

--- src/javascripts/components/boards/boards.js
+++ src/javascripts/components/boards/boards.js
@@ -4,7 +4,7 @@ const buildBoards = (board) => {
   domString += `<div id="${board.id}" class="card board-card">`;
   domString += '<div class="card-header d-flex flex-wrap">';
   domString += `<div class="align-self-center text-center col">${board.name}</div>`;
-  domString += '<button class="btn btn-danger delete-board"><i class="fas fa-trash delete-board"></i></button>';
+  domString += '<button class="btn btn-danger delete-board"><i class="fas fa-trash"></i></button>';
   domString += '</div>';
   domString += '<div class="card-body">';
   domString += `<p class="text-center">${board.description}</p>`;
