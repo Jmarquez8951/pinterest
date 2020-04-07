5c9707da983045342e083eee1d42756c5ca4564b
Jose Marquez <josesinmarquezpalmeros@gmail.com>
2020-04-07 18:06:59 -0500

    wip

--- src/javascripts/components/createNewObject/createNewObject.js
+++ src/javascripts/components/createNewObject/createNewObject.js
@@ -1,7 +1,14 @@
+const events = (boardId, newUrl) => {
+  console.error('', newUrl);
+};
+
 const newObject = () => {
-  console.error($('#new-pin').attr('class').toString().split(' ')[2]);
-  $('#newObjectModalLabel').html('New header');
-  $('#newObjectBody').html('Test');
+  const boardId = $('#new-pin').attr('class').toString().split(' ')[2];
+  console.error(boardId);
+  $('#newObjectModalLabel').html('<h2>New Pin</h2>');
+  $('#newObjectBody').html('<div class="form-group"><label for="inputImgURL">New Pin Url</label><input type="text" class="form-control" id="inputImgURL" placeholder="Enter Image Url"></div>');
+  const newUrl = $('#inputImgURL').val();
+  $('#save-btn').on('click', events(boardId, newUrl));
 };
 
 export default { newObject };
